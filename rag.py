853408cd6156181858b94f89f4fc73b0436c8b8c
Pragadeesh122 <pragan189@gmail.com>
2025-02-11 01:24:45 -0600

    intial model implementation

--- rag.py
+++ rag.py
@@ -1,5 +1,14 @@
 
+import asyncio
+from ollama import AsyncClient
 
 
-def main():
-       print()
+async def main():
+    response = await AsyncClient().generate(model='llama3.2', prompt="Whats the weather in mars", system="Answer in a sarcastic way",stream=True)
+
+    async for chunk in response:
+        print(chunk.response, end="", flush=True)
+
+asyncio.run(main())
+
+
