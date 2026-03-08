b1db7d8828642c7719503036a506485e9ef8944a
Pragadeesh122 <pragan189@gmail.com>
2026-03-08 18:56:10 -0500

    testing out the gemini api

--- hello.py
+++ hello.py
@@ -1,8 +1,16 @@
 import requests
+from google.genai import types
+from client import client
 
 
 def main():
     print("Hello from agenticrag!")
+
+    respone = client.models.generate_content(model="gemini-3-flash-preview",contents="Hey there")
+
+    print(respone.text)
+
+
 
 
 if __name__ == "__main__":
